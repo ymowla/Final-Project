8fe9627f349d83dc15c229b674b9a7090812c231
ymowla <32875156+ymowla@users.noreply.github.com>
2017-12-06 17:29:49 -0500

    final

---
+++ Yogeshwara Mowla final website/js/app.js
@@ -0,0 +1,34 @@
+$(document).foundation()
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// shows and hides filtered items
+$(".filter-simple-button").click(function() {
+  var value = $(this).attr('data-filter');
+  if(value === "all") {
+    $('.filter-simple-item').show('1000');
+  } else {
+    $(".filter-simple-item").not('.'+value).hide('3000');
+    $('.filter-simple-item').filter('.'+value).show('3000');
+  }
+});
+
+// changes active class on filter buttons
+$('.filter-simple-button').click(function () {
+  $(this).siblings().removeClass('is-active');
+  $(this).addClass('is-active');
+});
